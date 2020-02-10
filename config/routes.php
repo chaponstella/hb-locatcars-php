eb8654fba0527cf40edc59b68e8720e87896ceb1
chaponstella <chapon.stella01@gmail.com>
2020-02-10 15:59:49 +0100

    route service container

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,11 @@
 <?php
 
-use Bramus\Router\Router;
-
-$router = new Router;
-
+$router = $container->getRouter();
 
 // Quand je vais sur "/hello", j'effectue l'action suivante, qui est définie
 // dans la fonction anonyme notée juste après : function() { /* action */ } 
 $router->get('/hello', function() { 
-    echo "Hello world !";
+    echo "Makram";
 });
 
 $router->run(); # À ne jamais oublier sinon le routeur ne se lance pas !
